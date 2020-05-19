1de9dfa68478371fc3fb9e427d32c3918849c2b2
lixiang093214 <lixianggxu@163.com>
2020-05-19 20:51:03 +0800

    refector: remove for loop.

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -1,62 +1,60 @@
 package cn.xpbootcamp.gilded_rose;
 
 class GildedRose {
-    Product[] products;
+    Product product;
 
-    public GildedRose(Product[] products) {
-        this.products = products;
+    public GildedRose(Product product) {
+        this.product = product;
     }
 
     public void update_quality() {
-        for (int i = 0; i < products.length; i++) {
-            if (!products[i].name.equals("Aged Brie")
-                    && !products[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (products[i].quality > 0) {
-                    if (!products[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        products[i].quality = products[i].quality - 1;
+        if (!product.name.equals("Aged Brie")
+                && !product.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (product.quality > 0) {
+                if (!product.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    product.quality = product.quality - 1;
                     }
                 }
             } else {
-                if (products[i].quality < 50) {
-                    products[i].quality = products[i].quality + 1;
+            if (product.quality < 50) {
+                product.quality = product.quality + 1;
 
-                    if (products[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (products[i].sellIn < 11) {
-                            if (products[i].quality < 50) {
-                                products[i].quality = products[i].quality + 1;
+                if (product.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (product.sellIn < 11) {
+                        if (product.quality < 50) {
+                            product.quality = product.quality + 1;
                             }
                         }
 
-                        if (products[i].sellIn < 6) {
-                            if (products[i].quality < 50) {
-                                products[i].quality = products[i].quality + 1;
+                    if (product.sellIn < 6) {
+                        if (product.quality < 50) {
+                            product.quality = product.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!products[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                products[i].sellIn = products[i].sellIn - 1;
+        if (!product.name.equals("Sulfuras, Hand of Ragnaros")) {
+            product.sellIn = product.sellIn - 1;
             }
 
-            if (products[i].sellIn < 0) {
-                if (!products[i].name.equals("Aged Brie")) {
-                    if (!products[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (products[i].quality > 0) {
-                            if (!products[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                products[i].quality = products[i].quality - 1;
+        if (product.sellIn < 0) {
+            if (!product.name.equals("Aged Brie")) {
+                if (!product.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (product.quality > 0) {
+                        if (!product.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            product.quality = product.quality - 1;
                             }
                         }
                     } else {
-                        products[i].quality = 0;
+                    product.quality = 0;
                     }
                 } else {
-                    if (products[i].quality < 50) {
-                        products[i].quality = products[i].quality + 1;
+                if (product.quality < 50) {
+                    product.quality = product.quality + 1;
                     }
                 }
             }
-        }
     }
 }
--- src/test/java/cn/xpbootcamp/gilded_rose/GildedRoseTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/GildedRoseTest.java
@@ -64,7 +64,7 @@ class GildedRoseTest {
     void should_update_product_correctly(ProductFixture productFixture) {
         Product product = createProduct(productFixture.name, productFixture.sellIn, productFixture.quality);
 
-        new GildedRose(new Product[]{product}).update_quality();
+        new GildedRose(product).update_quality();
 
         Product expectedProduct = createProduct(productFixture.name, productFixture.updatedSellIn, productFixture.updatedQuality);
         assertThat(product.toString()).isEqualTo(expectedProduct.toString());
