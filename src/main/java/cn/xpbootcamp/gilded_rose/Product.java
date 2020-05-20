3d8017b78a3e4b6152c793766efb525b4a5e475c
lixiang093214 <lixianggxu@163.com>
2020-05-20 20:20:49 +0800

    refector: extract two method to update Quanlity for Baskstage Passes and update sellin except Sufuras.

--- src/main/java/cn/xpbootcamp/gilded_rose/Product.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Product.java
@@ -32,23 +32,13 @@ public class Product {
                 quality = quality + 1;
 
                 if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
+                    oneDayPassBackstagePasses();
                 }
             }
         }
 
         if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-            sellIn = sellIn - 1;
+            updateSellInExceptSufuras();
         }
 
         if (sellIn < 0) {
@@ -66,6 +56,24 @@ public class Product {
                 if (quality < 50) {
                     quality = quality + 1;
                 }
+            }
+        }
+    }
+
+    private void updateSellInExceptSufuras() {
+        sellIn = sellIn - 1;
+    }
+
+    private void oneDayPassBackstagePasses() {
+        if (sellIn < 11) {
+            if (quality < 50) {
+                quality = quality + 1;
+            }
+        }
+
+        if (sellIn < 6) {
+            if (quality < 50) {
+                quality = quality + 1;
             }
         }
     }
